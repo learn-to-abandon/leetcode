8218700682e3c9bc895e7482659c2491a715433d
learn-to-abandon <2587750084@qq.com>
2019-10-05 10:12:20 +0800

    56、57、81

---
+++ 1~100/51~60/56.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int len = intervals.size();
+        if (len<=1)
+            return intervals;
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for(int i=1;i<len;++i){
+            if(res.back()[1]>=intervals[i][0])
+                res.back()[1]=max(res.back()[1],intervals[i][1]);
+            else
+                res.push_back(intervals[i]);
+        }
+        return res;
+    }
+};
